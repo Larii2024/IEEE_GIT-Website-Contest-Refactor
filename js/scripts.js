c899b26b14987c80ff1ebf4e3802d33e6a380502
Larii2024 <53659954+Larii2024@users.noreply.github.com>
2020-08-19 12:56:37 +0530

    .

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,7 @@ $(document).ready(function(){
                     $("#carouselButton").children("span").addClass('fa-pause');                    
                 }
             });
-            $(".testimonial-carousel").slick({
-                 infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplay: false,
-                arrows: true,
-                prevArrow: $(".testimonial-carousel-controls .prev"),
-                nextArrow: $(".testimonial-carousel-controls .next")
-            });
+            
 
 
 
